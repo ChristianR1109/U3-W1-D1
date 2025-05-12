054ebe6ff2820efa64f0977afc8aaa1770599825
Christian Ragonese <christianragonese1313@gmail.com>
2025-05-12 17:45:48 +0200

    some fix

--- new-app/src/components/ImageComponent.jsx
+++ new-app/src/components/ImageComponent.jsx
@@ -3,8 +3,9 @@ import { Component } from "react";
 class ImageComponent extends Component {
   render() {
     return (
-      <div className="img container">
+      <div className="img-container">
         <img src={this.props.imgName} alt={this.props.imgAlt} width={300} />
+        <p>{this.props.customTitle}</p>
       </div>
     );
   }
